93809d8b232e4cec24907a5a1d4feb7957ef4da0
张强 <johnny@Johnny-MacBookPro.local>
2023-03-30 11:30:44 +0800

    add array circleArray 数组实现 环形队列

---
+++ src/com/johnny/queue/ArrayQueueTest.java
@@ -0,0 +1,114 @@
+package com.johnny.queue;
+
+import java.util.Scanner;
+
+/**
+ * Created on 2023/3/30 10:04.
+ *
+ * @author Johnny
+ */
+public class ArrayQueueTest {
+
+    public static void main(String[] args) {
+
+
+        int size = 3;
+        int real = 2;
+        System.out.println((2) % 4);
+
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+
+        System.out.println("数组队列 测试程序开始...");
+        System.out.println("输入a(add) 添加数据到队列");
+        System.out.println("输入g(get) 从队列获取一个数据");
+        System.out.println("输入l(length) 获取队列的长度");
+        System.out.println("输入s(show) 显示队列的头部");
+
+        boolean flag = true;
+        while (flag) {
+            Scanner scanner = new Scanner(System.in);
+            String operator = scanner.next();
+            switch (operator) {
+                case "a":
+                    System.out.println("请输入入队数据");
+                    String insertValue = scanner.next();
+                    arrayQueue.addQueue(Integer.parseInt(insertValue));
+                    break;
+                case "g":
+                    int queue = arrayQueue.getQueue();
+                    System.out.println(queue);
+                    break;
+                case "l":
+                    System.out.println(arrayQueue.size());
+                    break;
+                case "s":
+                    System.out.println(arrayQueue.showHeader());
+                    break;
+                case "exit":
+                    System.out.println("程序退出...");
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("未找到程序指令,请正确输入");
+            }
+        }
+    }
+
+    static class ArrayQueue {
+        int maxSize;
+        int front;
+        int real;
+        int[] array;
+
+        /**
+         * 构造一个 固定数量的 队列
+         *
+         * <p>front 指向队列头部的 前一个位置 real 指向队列尾部
+         *
+         * @param maxSize: 队列容量
+         */
+        public ArrayQueue(int maxSize) {
+            this.maxSize = maxSize;
+            // 初始化这个数组队列
+            array = new int[this.maxSize];
+            front = -1;
+            real = -1;
+        }
+
+        public boolean isFull() {
+            return real == maxSize - 1;
+        }
+
+        public boolean isEmpty() {
+            return front == real;
+        }
+
+        public void addQueue(int value) {
+            if (isFull()) {
+                throw new RuntimeException("队列满了 无法添加数据...");
+            }
+            //添加数据 指向队列尾部的指针 +1 然后存入数据
+            real++;
+            array[real] = value;
+        }
+
+        public int getQueue() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列是空 无法获取数据...");
+            }
+            front++;
+            return array[front];
+        }
+
+        public int size() {
+            return array.length;
+        }
+
+        public int showHeader() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列是空 无法获取数据...");
+            }
+            return array[front + 1];
+        }
+    }
+}
---
+++ src/com/johnny/queue/CircleArrayTest.java
@@ -0,0 +1,98 @@
+package com.johnny.queue;
+
+/**
+ * Created on 2023/3/30 11:05.
+ *
+ * <p>数组实现环形队列, 需要留一个空格 所以最多存放maxSize-1个数据
+ * https://blog.csdn.net/qq_44287198/article/details/116494219 看这个博客 里面的图 能够很容易理解
+ *
+ * @author Johnny
+ */
+public class CircleArrayTest {
+
+    public static void main(String[] args) {
+        CircleArray queue = new CircleArray(4);
+
+        System.out.println(queue.isEmpty());
+        // 先插入3个数据
+        queue.addQueue(10);
+        queue.addQueue(20);
+        queue.addQueue(30);
+        // 看下是否已经满了 true 已经满了 环形队列只能存储 maxSize -1 = 4-1= 3个数据
+        System.out.println(queue.isFull());
+        // 尝试再插入一个 抛已经满了
+        queue.addQueue(40);
+        System.out.println(queue.isFull());
+        //show 发现确实有一个空缺位置 arr[3] = null
+        queue.show();
+        //front = 0
+        System.out.println(queue.front);
+        //rear = 3  这个位置是null
+        System.out.println(queue.rear);
+
+        //取2个数据
+        System.out.println(queue.getQueue());
+        System.out.println(queue.getQueue());
+
+        queue.show();
+
+        System.out.println("-----------");
+        queue.addQueue(40);
+        queue.addQueue(50);
+        queue.show();
+
+        System.out.println(queue.front);
+        System.out.println(queue.rear);
+    }
+
+    static class CircleArray {
+        int maxSize;
+        int front;
+        int rear;
+        Object[] arr;
+
+        public CircleArray(int arrayMaxSize) {
+            this.maxSize = arrayMaxSize;
+            this.arr = new Object[maxSize];
+        }
+
+        public boolean isEmpty() {
+            return front == rear;
+        }
+
+        public boolean isFull() {
+            return (rear + 1) % maxSize == front;
+        }
+
+        public void addQueue(Object value) {
+            if (isFull()) {
+                System.out.println("队列已经满了");
+                return;
+            }
+            arr[rear] = value;
+            rear = (rear + 1) % maxSize;
+        }
+
+        public Object getQueue() {
+            if (isEmpty()) {
+                System.out.println("队列是空的 无法获取");
+                return null;
+            }
+            Object value = arr[front];
+            // 可置空 也可不管 等着覆盖
+            arr[front] = null;
+            front = (front + 1) % maxSize;
+            return value;
+        }
+
+        public void show() {
+            if (isEmpty()) {
+                System.out.println("队列是空的 无法获取");
+                return;
+            }
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println("arr[" + i + "] = " + arr[i]);
+            }
+        }
+    }
+}
